9ba31b1280dc78b696bd713ea20cc8ebe48d4b72
Guy Turner <guturner@users.noreply.github.com>
2017-01-19 19:57:21 -0500

    Potential Heroku / Stormpath apiKey workaround.

--- src/main/java/org/guy/rpg/dwg/config/StormpathConfig.java
+++ src/main/java/org/guy/rpg/dwg/config/StormpathConfig.java
@@ -5,7 +5,7 @@ import java.util.Properties;
 public class StormpathConfig extends Properties {
 
     public StormpathConfig() {
-        super.put("apiKey.id", System.getenv("STORMPATH_API_KEY_ID"));
-        super.put("apiKey.secret", System.getenv("STORMPATH_API_KEY_SECRET"));
+        super.put("apiKey.id", System.getenv("STORMPATH_CLIENT_APIKEY_ID"));
+        super.put("apiKey.secret", System.getenv("STORMPATH_CLIENT_APIKEY_SECRET"));
     }
 }
