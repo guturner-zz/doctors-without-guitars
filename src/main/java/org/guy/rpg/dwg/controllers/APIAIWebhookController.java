79305704407e977209b289a850c0ad42bfd6c7bb
Guy Turner <Guy Turner>
2017-01-25 16:36:15 -0500

    Adding additional API.AI responses for spell requests.

--- src/main/java/org/guy/rpg/dwg/controllers/APIAIWebhookController.java
+++ src/main/java/org/guy/rpg/dwg/controllers/APIAIWebhookController.java
@@ -22,8 +22,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class APIAIWebhookController {
 
+	private static final String SPELL_LISTS_URL = "http://www.d20pfsrd.com/magic/spell-lists-and-domains/spell-lists";
+	
 	private Map<String, String> classSpellsMap = new HashMap<String, String>() {{
-		put("bard", "Check out <a href='http://www.d20pfsrd.com/magic/spell-lists-and-domains/spell-lists---bard' target='_blank'>this list of Bard spells</a>!");
+		put("barbarian", "Barbarian's are brutes, they can't cast spells!");
+		put("bard", "Check out <a href='" + SPELL_LISTS_URL + "---bard' target='_blank'>this list of Bard spells</a>!");
+		put("cleric", "Sure, take a look at <a href='" + SPELL_LISTS_URL + "---cleric' target='_blank'>these Cleric spells</a>!");
+		put("druid", "I've got a bunch of <a href='" + SPELL_LISTS_URL + "---druid' target='_blank'>Druid spells right here</a>!");
+		put("fighter", "Uhh, Fighters don't cast spells buddy.");
+		put("monk", "Monks don't cast spells, were you looking for <a href='http://www.d20pfsrd.com/classes/unchained-classes/monk-unchained#TOC-Ki-Power-Su-' target='_blank'>a list of their Kis</a>?");
+		put("paladin", "Plenty of <a href='" + SPELL_LISTS_URL + "---paladin' target='_blank'>Paladin spells here</a>!");
+		put("ranger", "Sure thing, check out <a href='" + SPELL_LISTS_URL + "---ranger' target='_blank'>this list of Ranger spells</a>!");
+		put("rogue", "I'm afraid I can't do that. Rogues don't cast spells.");
+		put("sorcerer", "I've got plenty of <a href='" + SPELL_LISTS_URL + "---sorcerer-and-wizard' target='_blank'>Sorcerer spells right here</a>!");
+		put("wizard", "Hmm, I can't find any. Just kidding, check out <a href='" + SPELL_LISTS_URL + "---sorcerer-and-wizard' target='_blank'>this list of Wizard spells</a>!");
 	}};
 	
 	@Autowired
