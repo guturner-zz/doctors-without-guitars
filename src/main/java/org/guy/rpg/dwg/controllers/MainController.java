cab5d0355ddf10c96ead3cc04ca48c325b1a2408
Guy Turner <guturner@users.noreply.github.com>
2017-02-11 02:24:12 -0500

    Fixing NPE around character list when first viewing homepage.

--- src/main/java/org/guy/rpg/dwg/controllers/MainController.java
+++ src/main/java/org/guy/rpg/dwg/controllers/MainController.java
@@ -2,6 +2,7 @@ package org.guy.rpg.dwg.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.guy.rpg.dwg.models.db.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,11 @@ public class MainController extends BaseController {
 	@RequestMapping("/")
 	public String main(HttpServletRequest request, Model model) {
 		model.addAllAttributes(getAttributeMap(request));
-		model.addAttribute("characters", dbManager.getCurrentUser(request).getCharacters());
+		
+		User user = dbManager.getCurrentUser(request);
+		if (user != null) {
+			model.addAttribute("characters", user.getCharacters());
+		}
 		
 		return "main/main";
 	}
